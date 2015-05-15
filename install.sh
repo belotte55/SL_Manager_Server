31e57db788a357b110ea9e2da085f2835d00aebe
belotte55 <belotte1355@gmail.com>
2015-05-15 18:33:14 +0200

    Initial realise

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ echo 'Installation...'
 
 if [[ -e /usr/local/bin/SL_Manager_Server ]]; then
 	echo 'SL_Manager_Server has been found.'
-	echo 'Do you want to replace it with the new one ? (Y/n):'
+	echo -n 'Do you want to replace it with the new one ? (Y/n):'
 
 	read -e replace_directory
 
-	case $replace in
+	case $replace_directory in
 		"" | "Y" | "y" | "o" | "Yes" | "yes" | "O")
 			echo 'Purging previous files...'
 			sudo rm -r /usr/local/bin/SL_Manager_Server/*
@@ -30,6 +30,35 @@ else
 	echo 'Copying files...'
 	sudo cp sources/* /usr/local/bin/SL_Manager_Server
 fi
+
+echo -n 'Do you want the server starts when the Raspberry starts ? (y/N):'
+
+read -e create_daemon
+
+case $create_daemon in
+	"Y" | "y" | "o" | "Yes" | "yes" | "O")
+		if [[ ! -f /etc/init.d/SL_Manager_Server_Daemon ]]; then
+			echo "
+#!/bin/bash
+case \"\$1\" in 
+	start)
+		touch /home/pi/ZZZzzzzzzzzzzzzzzz
+		#sudo python /usr/local/bin/SL_Manager_Server/server.py
+		;;
+	*)
+		;;
+esac" > SL_Manager_Server_Daemon
+			sudo mv SL_Manager_Server_Daemon /etc/init.d/
+			echo 'File created to /etc/init.d/'
+
+			sudo chmod +x /etc/init.d/SL_Manager_Server_Daemon
+			sudo update-rc.d SL_Manager_Server_Daemon defaults
+		fi
+		;;
+	*)
+		exit 0
+		;;
+esac
 
 echo 'Done.'
 
